d13653f5397be93d328fef5798ab6be0be11edff
M Alvin Ramadhan <malvinr789@gmail.com>
2018-12-17 19:59:32 +0700

    add okhttp

--- app/src/main/java/malvinr/me/tokohijau/network/ApiClient.kt
+++ app/src/main/java/malvinr/me/tokohijau/network/ApiClient.kt
@@ -1,16 +1,32 @@
 package malvinr.me.tokohijau.network
 
 import malvinr.me.tokohijau.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApiClient {
 
     companion object {
+        private val REQUEST_TIME_OUT: Long = 30
+
         fun builder(): Retrofit {
+            val loggingInterceptor = HttpLoggingInterceptor()
+            loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+            val client = OkHttpClient.Builder()
+                .connectTimeout(REQUEST_TIME_OUT, TimeUnit.SECONDS)
+                .writeTimeout(REQUEST_TIME_OUT, TimeUnit.SECONDS)
+                .readTimeout(REQUEST_TIME_OUT, TimeUnit.SECONDS)
+                .addInterceptor(loggingInterceptor)
+                .build()
+
             return Retrofit.Builder()
                 .baseUrl(BuildConfig.BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
